d76dc3e6d8b934845ddf14edde8318c5f5e2e35a
Andrew van Rooyen <andrew@andrewvr.dev>
2022-04-18 17:36:33 +0200

    fix: explain the /bookshelf prefix to next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/bookshelf',
+  assetPrefix: '/bookshelf/',
   images: {
     domains: ['i.gr-assets.com'],
   },
